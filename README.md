516d671017d2e74b55122bbcc5d17ff19ee781bb
gamitej <gamitejpratapsingh998@gmail.com>
2022-03-20 21:53:54 +0530

    postgres code

---
+++ README.md
@@ -0,0 +1,11 @@
+## POSTGRESQL PYTHON SCRIPT 
+
+### Script Includes :-
+
+1. Connection to pgadmin.
+2. Use of cursor to execute different query/commands.
+3. CRUD operation query/code.
+4. Relation between two table it can be ( one-to-one ) or ( many-to-one ).
+
+
+
---
+++ postgres.py
@@ -0,0 +1,75 @@
+import psycopg2
+
+def createTable(cursor):
+    command = '''
+        CREATE TABLE users (
+            id serial,
+            firstname VARCHAR(25) NOT NULL,
+            lastname VARCHAR(25) NOT NULL,
+            PRIMARY KEY (id)
+        )
+        ''' 
+    cursor.execute(command)
+    print('Table created successfully')
+
+def createTableRelation(cursor):
+    command = '''
+        CREATE TABLE address (
+            user_id int NOT NULL,
+            city VARCHAR(30) NOT NULL,
+            state VARCHAR(30) NOT NULL,
+            PRIMARY KEY (user_id),
+            CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+        )
+        ''' 
+    cursor.execute(command)
+    print('Table created successfully')
+
+
+def insertData(cursor,user_id,city,state):
+    insert_query = "insert into address (user_id,city,state) values(%s,%s,%s)"
+    record_to_insert = (user_id,city,state)
+    cursor.execute(insert_query,record_to_insert) 
+    print('Insertion successfull')
+
+def updateTableData(cursor):
+    cursor.execute("UPDATE users SET age = 23 where id = 3")
+    print('Data Updated Successfully')
+
+#  Note -> while deleting data from table make sure you first delete the fk one data then the pk one later
+def deleteFromTable(cursor):
+    cursor.execute("DELETE FROM users WHERE id = 2")
+    print('Data Deleted Successfully')
+
+# Note -> while altering column if column is not null then define it as default some_value 
+def alterColumn(cursor):
+    cursor.execute("ALTER TABLE users ADD COLUMN age int ")
+    print('Column Added Successfully')
+
+def getRecords(cursor):
+    cursor.execute('select * from address')
+    record = cursor.fetchall()
+    print('Records',record)
+
+
+Connection = None 
+try:
+    connection = psycopg2.connect(user='postgres',password='postgres',host='127.0.0.1',port='5432',database='postgres')
+    cursor  =  connection.cursor()
+    print('Connected to BD ->\n')
+    #createTable(cursor)
+    #createTableRelation(cursor)
+    #insertData(cursor,3,'Satna','M.P')
+    #updateTableData(cursor)
+    #deleteFromTable(cursor)
+    #alterColumn(cursor)
+    getRecords(cursor)
+    connection.commit()
+except (Exception,psycopg2.Error) as error:
+    print(error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print('\nDB connection closed ->')
+print('Done :)')
